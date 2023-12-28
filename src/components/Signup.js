54aa5378d070bd289cecbc09e1660bf7c6428c6c
m-Shehzaib <98905825+muhammadshehzaib@users.noreply.github.com>
2023-12-28 12:43:20 +0500

    fronend deploying on vercel

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { ToastContainer, toast } from "react-toastify";
 import { useNavigate } from "react-router-dom"; // Import useNavigate
 import "react-toastify/dist/ReactToastify.css";
 import Navbar from "./Navbar/Navbar";
